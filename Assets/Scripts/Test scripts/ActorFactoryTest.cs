cd5b5e95f10f79fbabd70421f8689c940175f0bc
chromesome <71896076+chromesome@users.noreply.github.com>
2020-09-25 22:03:23 -0300

    Actor Factory
    clases actor y factory

---
+++ Assets/Scripts/Test scripts/ActorFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorFactoryTest : MonoBehaviour
+{
+    [SerializeField] private ActorFactory _actorFactory;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            _actorFactory.CreateNewActor(0);
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            _actorFactory.CreateNewActor(1);
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            _actorFactory.CreateNewActor(2);
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            _actorFactory.CreateNewActor(3);
+        }
+    }
+}
