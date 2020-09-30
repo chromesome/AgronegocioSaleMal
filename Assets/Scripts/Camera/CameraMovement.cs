374b2da09ddbe8a3312ce78ad67b26d77eff4eea
chromesome <71896076+chromesome@users.noreply.github.com>
2020-09-30 16:50:17 -0300

    Camera movement
    Added camera movement
    WASD - position
    Mouse wheel - zoom in/out

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float panSpeed = 2f;
+    public float scrollSpeed = 200f;
+
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        #region camera movement
+        if (Input.GetKey("w"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+        #endregion
+
+        // Camera zoom
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize -= scroll * scrollSpeed * Time.deltaTime;
+    }
+}
