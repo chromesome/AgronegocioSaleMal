b48d0edd96d4efc74d3d54df5b6487c67e9e6dfc
chromesome <71896076+chromesome@users.noreply.github.com>
2020-10-02 19:41:06 -0300

    fix level

--- Assets/Scripts/Actors/Fire.cs
+++ Assets/Scripts/Actors/Fire.cs
@@ -2,7 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : Actor
+public class Fire : Actor, IHarmful, IDestructible
 {
+    public int GetCurrentHealth()
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public int GetMaxHealth()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int ReceiveDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void MakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
 }
