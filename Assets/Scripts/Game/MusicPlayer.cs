c356ee4f150f684f11c25e7008f26099cd8d8fe7
chromesome <71896076+chromesome@users.noreply.github.com>
2020-10-04 16:36:38 -0300

    Music manager

---
+++ Assets/Scripts/Game/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public List<AudioClip> musicAudioClips;
+    public AudioSource audioSource;
+
+    const int MUSIC_CHILL = 0;
+
+    void Start()
+    {
+        PlayMusicLoop(MUSIC_CHILL);
+    }
+
+    public void PlayMusicLoop(int musicIndex)
+    {
+        audioSource.clip = musicAudioClips[musicIndex];
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
